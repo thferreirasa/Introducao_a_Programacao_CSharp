8d45dc4f1f50da26e848caeb22334b905295faf3
Thaís <thferreirasa@gmail.com>
2024-05-05 02:19:46 -0300

    estudos repeticao do while e for

---
+++ Estudos/repeticao-do-while/repeticao-do-while/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Crie um algoritmo para somar e exibir os valores que o usuário digitar, porém, quando ele digitar o número
+            // zero, o loop deve acabar e mostrar o resultado final da soma.
+
+            int valorInicial = 0;
+            int numero = 0;
+        
+            do
+            {                
+                Console.WriteLine("Insira um valor:");
+                numero = int.Parse(Console.ReadLine());
+
+                int soma = numero + valorInicial;
+                Console.WriteLine("O resultado é " + soma);
+
+                valorInicial = soma;
+            } while (numero != 0);
+            */
+
+            /*
+            // Crie um algoritmo que não permite que o usuário informe um valor menor que 100. Se ele informar um valor
+            // menor, deve mostrar uma mensagem e solicitar novamente. Se ele informar maior, ele mostra mensagem de sucesso.
+
+            int numero = 100;
+
+            do
+            {
+                Console.WriteLine("Informe um valor maior que 100:");
+                numero = int.Parse(Console.ReadLine());
+            } while (numero < 100);
+
+            Console.WriteLine("O número informado é maior que 100!");
+            */
+
+            /*
+            // Construa um algoritmo para um pequeno game, no qual um jogador entra com um número e o outro jogador deve
+            // adivinhar o número informado.
+            // As regras são:
+            // 1. O algoritmo deve pedir para o jogador desafiante informar um valor numérico;
+            // 2. Na sequência o algoritmo deve pedir para o jogador desafiado informar um valor.
+            // a) Se o valor informado for igual ao valor do desafiante, o algoritmo deve exibir a mensagem de sucesso
+            // b) Se o valor informado for diferente, o sistema deve informar que está incorreto e deve pedir um novo valor
+
+            Console.WriteLine("Jogador Desafiante, escolha um número entre 1 e 9:");
+            int desafiante = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            int desafiado = desafiante;
+
+            do
+            {         
+                Console.WriteLine("Jogador Desafiado, qual sua aposta?");
+                desafiado = int.Parse(Console.ReadLine());
+
+            } while (desafiante != desafiado);
+
+            Console.WriteLine("Muito bem! O número era " + desafiante);
+            */
+        }
+    }
+}
---
+++ Estudos/repeticao-for/repeticao-for/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace repeticaofor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Crie um algoritmo para exibir os números de 1 a 10 em ordem crescente.
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /*
+            // Crie um algoritmo para exibir os números de 1 a 10 em ordem decrescente.
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /*
+            // Crie um algoritmo para exibir a tabuada do 7
+            for (int i = 1; i <= 9; i++)
+            {
+                int multiplicacao = 7 * i;
+
+                Console.WriteLine("7 x {0} = {1}", i, multiplicacao);
+            }
+            */
+
+
+            /*
+            // Crie um algoritmo onde o usuário vai informar um número inteiro e o
+            // sistema vai imprimir a tabuada desse numero
+
+            Console.WriteLine("Informe o número da tabuada que você quer ver:");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                int multiplicacao = numero * i;
+                Console.WriteLine("{0} x {1} = {2}", numero, i, multiplicacao);
+            }
+            */
+
+            /*
+            // Construa um algoritmo onde o usuário vai informar um número entre
+            // 1 e 100, e o sistema deve mostrar todos os números sequenciais de
+            // 0 até o número informado.
+            Console.WriteLine("Informe um número entre 0 e 100:");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numero; i++)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            /*
+            // Construa um algoritmo onde o usuário vai informar o número final e 
+            // o multiplicador e o sistema deve mostrar de 0 até o número final e
+            // somando o multiplicador
+            Console.WriteLine("Informe o multiplicador:");
+            int multiplicador = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o número final");
+            int numeroFinal = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numeroFinal; i = i + multiplicador)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+        }
+    }
+}
